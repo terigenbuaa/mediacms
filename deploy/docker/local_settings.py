c89d6cabaf496ef444083458ff7e6931ddc018c7
teruun <teruun@163.com>
2023-11-25 10:55:49 +0800

    config changes

--- deploy/docker/local_settings.py
+++ deploy/docker/local_settings.py
@@ -1,6 +1,6 @@
 FRONTEND_HOST = 'http://localhost'
 PORTAL_NAME = 'MediaCMS'
-SECRET_KEY = 'ma!s3^b-cw!f#7s6s0m3*jx77a@riw(7701**(r=ww%w!2+yk2'
+SECRET_KEY = 'ma!s3^b-cw!f#7s6s0m3*jx77a@riwteruunsdflkasdfasd'
 POSTGRES_HOST = 'db'
 REDIS_LOCATION = "redis://redis:6379/1"
 
@@ -32,3 +32,6 @@ CELERY_RESULT_BACKEND = BROKER_URL
 MP4HLS_COMMAND = "/home/mediacms.io/bento4/bin/mp4hls"
 
 DEBUG = False
+
+PORTAL_NAME = 'tamir'
+CAN_ADD_MEDIA = "advancedUser"
